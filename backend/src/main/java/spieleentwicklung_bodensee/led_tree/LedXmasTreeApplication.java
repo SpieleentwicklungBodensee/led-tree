21ab37fcf463a07ab0b657a70af4fde83528c8fa
KeTr <kevintremmel@gmx.de>
2024-09-26 21:39:18 +0200

    empty backend application

---
+++ backend/src/main/java/spieleentwicklung_bodensee/led_tree/LedXmasTreeApplication.java
@@ -0,0 +1,13 @@
+package spieleentwicklung_bodensee.led_tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedXmasTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedXmasTreeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/spieleentwicklung_bodensee/led_tree/LedXmasTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package spieleentwicklung_bodensee.led_tree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedXmasTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
